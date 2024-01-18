a3a265a14dd90c21cd33ced5e5668b162bc0c980
Antoine <antoine.gourgue@epitech.eu>
2024-01-18 17:02:25 +0100

    feat: init vuejs & npm & LoginView & Input and Button compenents

---
+++ src/components/common/LoginInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <label for="email"
+           class="block text-sm font-medium leading-6 text-gray-900">Email</label>
+
+    <div class="mt-2">
+      <input id="email"
+             name="email"
+             type="email"
+             autocomplete="email"
+             required
+             class= "border-indigo-600 block w-full rounded-md border-2 py-1.5 text-gray-900 shadow-sm  placeholder:text-gray-400 pl-[14px] sm:text-sm sm:leading-6">
+    </div>
+  </div>
+
+  <div>
+    <div class="flex items-center justify-between">
+      <label for="password"
+             class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+    </div>
+    <div class="mt-2">
+      <input id="password"
+             name="password"
+             type="password"
+             autocomplete="current-password"
+             required
+             class= "border-indigo-600 block w-full rounded-md border-2 py-1.5 text-gray-900 shadow-sm  placeholder:text-gray-400 pl-[14px] sm:text-sm sm:leading-6">
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <img class="mx-auto h-24 w-auto" src="/EchoConnectLogo.png" alt="EchoConnect">
+      <h2 class="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+    </div>
+    <div class="mt-6 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form class="space-y-6" action="#" method="POST">
+
+        <LoginInput/>
+        <LoginButton/>
+        <div class="text-sm flex justify-center">
+          <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import LoginInput from '@/components/common/LoginInput.vue';
+import LoginButton from "@/components/common/LoginButton.vue";
+
+</script>
