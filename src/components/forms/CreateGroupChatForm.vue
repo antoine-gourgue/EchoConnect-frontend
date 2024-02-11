c7bbcd6572a4ed092689d8ffa0d5b994ede7a6b1
Antoine <antoine.gourgue@epitech.eu>
2024-02-11 12:57:47 +0100

    fix: design in generalchanel

--- src/components/forms/CreateGroupChatForm.vue
+++ src/components/forms/CreateGroupChatForm.vue
@@ -1,20 +1,28 @@
 <template>
-  <div class="chat-container">
-    <div class="messages-container">
-      <div v-for="message in messages" :key="message.id" class="message">
-<!--        <img :src="message.user.userImage" class="user-image" alt="User Image">-->
-        <div class="message-content">
-          <div class="username">{{ message.user.username }}</div>
-          <div class="text">{{ message.text }}</div>
+  <div class="flex flex-col h-screen">
+    <div class="flex-grow overflow-auto">
+      <div class="p-4 space-y-4 flex flex-col-reverse">
+        <div v-for="message in messages" :key="message.id" :class="{'justify-end': isMessageFromCurrentUser(message.user.id), 'justify-start': !isMessageFromCurrentUser(message.user.id)}" class="flex">
+          <div class="max-w-[60%] rounded-lg p-2" :class="{'bg-green-200': isMessageFromCurrentUser(message.user.id), 'bg-gray-200': !isMessageFromCurrentUser(message.user.id)}">
+            <div class="font-bold">{{ message.user.username }}</div>
+            <div>{{ message.text }}</div>
+          </div>
         </div>
       </div>
     </div>
-    <div class="message-input">
-      <input v-model="newMessage" type="text" placeholder="Écrire un message..." />
-      <button @click="sendMessage">Envoyer</button>
+    <div class="p-4">
+      <div class="flex space-x-2">
+        <input v-model="newMessage" type="text" placeholder="Écrire un message..."  class= "border-gray-600 focus:border-indigo-700 outline-none block w-full rounded-md border-2 py-1.5 text-gray-900 shadow-sm
+             placeholder:text-gray-400 pl-[14px] sm:text-sm sm:leading-6">
+        <button @click="sendMessage" class="flex w-[150px] justify-center rounded-md bg-[#4341C0] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+          Envoyer
+        </button>
+      </div>
     </div>
   </div>
 </template>
+
+
 
 <script setup>
 import {computed, ref} from 'vue';
@@ -28,6 +36,9 @@ const currentUser = computed(() => {
   return JSON.parse(localStorage.getItem('user'));
 });
 
+const isMessageFromCurrentUser = (messageUserId) => {
+  return messageUserId === currentUser.value.id;
+};
 
 socket.on('receiveMessage', message => {
   messages.value.push(message);
