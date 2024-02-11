a3bdaf34275ce1ba0f44be9d7e4de05b324c4149
Antoine <antoine.gourgue@epitech.eu>
2024-02-11 13:18:44 +0100

    fix: design in generalchanel and message send

--- src/components/forms/CreateGroupChatForm.vue
+++ src/components/forms/CreateGroupChatForm.vue
@@ -1,20 +1,29 @@
 <template>
   <div class="flex flex-col h-screen">
     <div class="flex-grow overflow-auto">
-      <div class="p-4 space-y-4 flex flex-col-reverse">
-        <div v-for="message in messages" :key="message.id" :class="{'justify-end': isMessageFromCurrentUser(message.user.id), 'justify-start': !isMessageFromCurrentUser(message.user.id)}" class="flex">
-          <div class="max-w-[60%] rounded-lg p-2" :class="{'bg-green-200': isMessageFromCurrentUser(message.user.id), 'bg-gray-200': !isMessageFromCurrentUser(message.user.id)}">
+      <div class="p-4 space-y-4 flex flex-col">
+        <div v-for="message in messages" :key="message.id" :class="{'justify-end': isMessageFromCurrentUser(message.user.id), 'justify-start': !isMessageFromCurrentUser(message.user.id)}" class="flex ">
+          <div class="max-w-[60%] rounded-lg p-2" :class="{'bg-indigo-500': isMessageFromCurrentUser(message.user.id), 'bg-gray-200': !isMessageFromCurrentUser(message.user.id)}">
             <div class="font-bold">{{ message.user.username }}</div>
-            <div>{{ message.text }}</div>
+            <div class="break-words">{{ message.text }}</div>
+            <div class="timestamp text-xs text-right mt-2">
+              {{ formatDate(message.timestamp) }}
+            </div>
           </div>
         </div>
       </div>
     </div>
     <div class="p-4">
       <div class="flex space-x-2">
-        <input v-model="newMessage" type="text" placeholder="Écrire un message..."  class= "border-gray-600 focus:border-indigo-700 outline-none block w-full rounded-md border-2 py-1.5 text-gray-900 shadow-sm
-             placeholder:text-gray-400 pl-[14px] sm:text-sm sm:leading-6">
-        <button @click="sendMessage" class="flex w-[150px] justify-center rounded-md bg-[#4341C0] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+        <input v-model="newMessage"
+               type="text" placeholder="Écrire un message..."
+               class= "border-gray-600 focus:border-indigo-700 outline-none block w-full rounded-md border-2 py-1.5 text-gray-900 shadow-sm
+             placeholder:text-gray-400 pl-[14px] sm:text-sm sm:leading-6"
+             @keydown.enter="sendMessage">
+        <button @click="sendMessage"
+                class="flex w-[150px] justify-center rounded-md bg-[#4341C0] px-3 py-1.5 text-sm font-semibold leading-6 text-white
+                shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
+                focus-visible:outline-indigo-600">
           Envoyer
         </button>
       </div>
@@ -43,6 +52,19 @@ const isMessageFromCurrentUser = (messageUserId) => {
 socket.on('receiveMessage', message => {
   messages.value.push(message);
 });
+
+function formatDate(timestamp) {
+  const date = new Date(timestamp);
+  return date.toLocaleString('fr-FR', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  });
+}
+
 
 const sendMessage = () => {
   if (newMessage.value.trim() !== '') {
