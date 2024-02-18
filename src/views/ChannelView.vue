c076f1fb5a8ea1a3a80626d822c84c587b6e90af
Antoine <antoine.gourgue@epitech.eu>
2024-02-18 01:15:12 +0100

    fix: delete hover delete channel

--- src/views/ChannelView.vue
+++ src/views/ChannelView.vue
@@ -17,7 +17,7 @@
           </button>
 
           <button @click="deleteChannel"
-                  class="mr-2 justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                  class="mr-2 justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
             <span class="hidden sm:inline">
               Supprimer le groupe
             </span>
